d617fbb07931a19cacb07e6ab862ac2f14a8b939
Alberto Chávez Pérez <alberto@francisco.nu>
2019-12-26 00:05:32 +0100

    pingController converts res to JSON

--- controllers/pingController.js
+++ controllers/pingController.js
@@ -1,4 +1,6 @@
 module.exports = function pingController(req, res, next) {
     // request, response, next will be used for error handling
-    res.send('pong');
+    // A JSON text is a serialized object or array. res.send doesn't convert non-objects or non-arrays to JSON.
+    // res.json converts also not valid JSON, like strings, null, undefined
+    res.json('pong');
 };
